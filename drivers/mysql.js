843f96353ba6e00eb2eb595c739807e8a4cd7d6f
Aaron Corso <aaron@corsoa.com>
2017-11-04 17:01:39 -0400

    Add static middleware

--- drivers/mysql.js
+++ drivers/mysql.js
@@ -1,13 +1,12 @@
 'use strict';
 const mysql = require('mysql');
 
-console.log(JSON.stringify(process.env, null, 4));
 var connection = mysql.createConnection({
    host: process.env.MYSQL_HOST,
    user: process.env.MYSQL_USER,
    password: process.env.MYSQL_PASSWORD,
    database : process.env.MYSQL_DB || 'joy',
-   debug: true
+   debug: process.env.MYSQL_DEBUG || false
 });
 
 connection.connect(function(err) {
