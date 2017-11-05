722c4a848ca862fec983fa08180bc9a22a2498e2
Aaron Corso <aaron@corsoa.com>
2017-11-05 12:04:45 -0500

    More UI work

--- public/js/all.js
+++ public/js/all.js
@@ -1,3 +1,21 @@
+if (window.location.href.indexOf('igoal') !== -1) {
+  setTimeout(() => {
+    var customer_id = parseInt(getParameterByName('customer_id'));
+    JSON.parse(localStorage.authorizedUsers).forEach((user) => {
+      if (user.customer_id === customer_id) {
+        $('#igoal-container').prepend(`
+        <a href="#" data-ix="new-interaction-2" class="link-2 visible-link">${user.first_name}'s Goal</a>
+        `);        
+      }
+    });
+    //load the available merchant categories
+    $.ajax({
+      url: '/merchant-categories'
+    }).done((categories) => {
+      console.log(categories);
+    });
+  }, 500);
+}
 if (window.location.href.indexOf('choose-account') !== -1) {
 $.ajax({
   url: '/list-accounts'
@@ -8,10 +26,34 @@ $.ajax({
   console.log(data);
   });
 }
-window.authorizedUsers = [];
+else {
+  //the index page.
+  $.ajax({
+    url: '/authorized-users/' + localStorage.account_id
+  }).done((data) => {
+    localStorage.authorizedUsers = JSON.stringify(data);
+    data.forEach((authorizedUser, key) => {
+      var userName = authorizedUser.first_name;
+      if (authorizedUser.is_primary) {
+        //todo: unspecified gender
+        if (authorizedUser.gender === 'Male') {
+          $('#user-container').prepend(generateMainProfile('imgpadre', userName));
+        }
+        else {
+          $('#user-container').prepend(generateMainProfile('imgmadre', userName));
+        }
+      }
+      else {
+        //$('#authorized-user-container').prepend(generateAuthorizedUserProfile(authorizedUser.gender, authorizedUser.first_name, key + 1));
+        $('#auth' + (key)).html('<span class="authUsername">' + userName);
+      }
+    });
+  });
+}
+
 function goToGoal(pos) {
-  var customer_id = window.authorizedUsers[pos].customer_id;
-  window.location.href = '/igoal?customer_id=' + customer_id;
+  var customer_id = JSON.parse(localStorage.authorizedUsers)[pos].customer_id;
+  window.location.href = `/igoal?customer_id=${customer_id}&account_id=${localStorage.account_id}`;
 }
 
 function getParameterByName(name, url) {
@@ -23,13 +65,15 @@ function getParameterByName(name, url) {
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
-var account_id = getParameterByName('account_id') || 100700000
+localStorage.account_id = getParameterByName('account_id') || 100700000
+
 var generateMainProfile = (className, userName) => {
 return `<div class="mainProfileWrapper">
     <figure data-ix="new-interaction-7" class="${className}"></figure>
     <span class="mainProfileName">${userName}</span>
   </div>`;
 };
+
 var generateAuthorizedUserProfile = (gender, first_name, pos) => {
   let genderClass = '';
   let posClass = '';
@@ -56,24 +100,4 @@ var generateAuthorizedUserProfile = (gender, first_name, pos) => {
   }
   return `<div data-ix="new-interaction-4" class="${genderClass} ${posClass}"></div>`;
 };
-$.ajax({
-  url: '/authorized-users/' + account_id
-}).done((data) => {
-  window.authorizedUsers = data;
-  data.forEach((authorizedUser, key) => {
-    var userName = authorizedUser.first_name;
-    if (authorizedUser.is_primary) {
-      //todo: unspecified gender
-      if (authorizedUser.gender === 'Male') {
-        $('#user-container').prepend(generateMainProfile('imgpadre', userName));
-      }
-      else {
-        $('#user-container').prepend(generateMainProfile('imgmadre', userName));
-      }
-    }
-    else {
-      //$('#authorized-user-container').prepend(generateAuthorizedUserProfile(authorizedUser.gender, authorizedUser.first_name, key + 1));
-      $('#auth' + (key)).html('<span class="authUsername">' + userName);
-    }
-  });
-});
+
