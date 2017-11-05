8fabb83511246f9a8ad68535ebceb41f9a242a20
Aaron Corso <aaron@corsoa.com>
2017-11-04 20:58:34 -0400

    Add these merchant lists plus ETL loader

---
+++ etl/process.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const csv = require('fast-csv');
+const stream = fs.createReadStream('merchant_list.csv');
+require('dotenv').config({ path: '../.env'});
+const driver = require('../drivers/mysql');
+var csvStream = csv()
+    .on("data", function(data){
+      console.log(typeof(data));
+      const sql = 'INSERT INTO merchants(mcc_description) VALUES(?)';
+       driver.query({
+          sql: sql,
+          values: [ data[0] ]
+       }, ((err, results) => {
+          console.error(err);
+         console.log(results);
+       }));
+    })
+    .on("end", function(){
+         console.log("done");
+    });
+
+stream.pipe(csvStream);
