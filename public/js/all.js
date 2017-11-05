0d61498db57ef7fa7cc626907389443ff8cebe78
Aaron Corso <aaron@corsoa.com>
2017-11-05 12:49:47 -0500

    Get the mygoal page working with real data

--- public/js/all.js
+++ public/js/all.js
@@ -1,4 +1,39 @@
-if (window.location.href.indexOf('igoal') !== -1) {
+if (window.location.href.indexOf('mygoal') !== -1) {
+  var generateBudgetHTML = (budget, i) => {
+    var generateIcon = (spent, max) => {
+      if (spent / max < .5) {
+        return 'happy';
+      }
+      else if (spent / max < 1) {
+        return 'confused';
+      }
+      else {
+        return 'sad-1';
+      }
+    };
+      return `<div class="form-block-3 w-form">
+          <form id="email-form-${i}" name="email-form-${i}" data-name="" class="form-${i}"><label for="" class="field-label-4">${budget.mcc_segment_description}</label>
+            <div class="div-block-12 w-clearfix">
+              <div class="text-block-5">$ ${budget.spentAmount} </div>
+              <div class="text-block-5 txtbarra">/</div>
+              <div class="text-block-6">$ ${budget.max_consume}</div>
+              <div class="div-block-11">
+                <img src="images/${generateIcon(budget.spentAmount, budget.max_consume)}.png" class="image-7">
+              </div>
+            </div>
+          </form>
+        </div>`;
+  };
+  var customer_id = localStorage.customer_id || 100730000;
+  $.ajax({
+    url: `/budget?customer_id=${customer_id}&date_from=09/01/2017&date_to=09/30/2017`
+  }).done((budgetData) => {
+    budgetData.forEach((budget, key) => {
+      $('#mygoal-container').prepend(generateBudgetHTML(budget, key));
+    });
+  });
+}
+else if (window.location.href.indexOf('igoal') !== -1) {
   setTimeout(() => {
     var customer_id = parseInt(getParameterByName('customer_id'));
     JSON.parse(localStorage.authorizedUsers).forEach((user) => {
@@ -17,7 +52,7 @@ if (window.location.href.indexOf('igoal') !== -1) {
     });
   }, 500);
 }
-if (window.location.href.indexOf('choose-account') !== -1) {
+else if (window.location.href.indexOf('choose-account') !== -1) {
 $.ajax({
   url: '/list-accounts'
 }).done((data) => {
